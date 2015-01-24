ecbd706b2904be934fe5422181e9f3ede52b0318
tiltshiftfocus <limzming@gmail.com>
2015-01-24 21:54:57 +0800

    Completed 'add' command

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -1,4 +1,6 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -13,18 +15,45 @@ public class TextBuddy {
         while(true) {
             System.out.print("command: ");
             String userCommand = sc.nextLine();
-            processCommand(userCommand);
+            executeCommand(userCommand, currentFile);
         }
 
     }
 
-    private static void processCommand(String userCommand) {
+    private static void executeCommand(String userCommand, File currentFile) {
         String command = getFirstWord(userCommand);
-
+        
         if(command.equals("add")){
-
+        	add(userCommand, currentFile);
         }
     }
+
+	private static void add(String userCommand, File currentFile) {
+		String textToAdd = removeFirstWord(userCommand);
+		
+		BufferedWriter outToFile;
+		
+		try {
+			outToFile = new BufferedWriter(new 
+					FileWriter(currentFile.getName(),true));
+			
+			if(!isFileEmpty(currentFile)){
+				outToFile.newLine();
+			}
+			
+			outToFile.write(textToAdd);
+			outToFile.close();
+			System.out.println("added to " + currentFile.getName()
+					+ ": \"" + textToAdd + "\"");
+			
+		} catch (IOException e) {
+			System.out.println("Error writing to file");
+		}
+	}
+
+	private static boolean isFileEmpty(File currentFile) {
+		return currentFile.length()<=0;
+	}
 
     private static void showWelcomeMessage(String arg) {
         System.out.println("Welcome to TextBuddy. " + arg + " is ready for use.");
