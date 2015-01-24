564d0b3af7d7a7d5b32eac673abb35c87ecaf8b2
tiltshiftfocus <limzming@gmail.com>
2015-01-24 21:13:27 +0800

    Add 'add' command

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -1,11 +1,33 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class TextBuddy {
 
     public static void main(String[] args) {
 	    File currentFile = openFile(args[0]);
-        System.out.println("Welcome to TextBuddy. " + args[0] + " is ready for use.");
+        Scanner sc = new Scanner(System.in);
+
+        showWelcomeMessage(args[0]);
+
+        while(true) {
+            System.out.print("command: ");
+            String userCommand = sc.nextLine();
+            processCommand(userCommand);
+        }
+
+    }
+
+    private static void processCommand(String userCommand) {
+        String command = getFirstWord(userCommand);
+
+        if(command.equals("add")){
+
+        }
+    }
+
+    private static void showWelcomeMessage(String arg) {
+        System.out.println("Welcome to TextBuddy. " + arg + " is ready for use.");
     }
 
     private static File openFile(String fileName) {
@@ -20,5 +42,13 @@ public class TextBuddy {
             }
         }
         return file;
+    }
+
+    private static String removeFirstWord(String userCommand) {
+        return userCommand.replace(getFirstWord(userCommand), "").trim();
+    }
+
+    private static String getFirstWord(String userCommand) {
+        return userCommand.trim().split("\\s+")[0];
     }
 }
